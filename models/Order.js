445a2111189a37c51d2bb685f2df3a24d9baa756
Hanish SS <hanishgowda7795@gmail.com>
2026-03-29 10:25:21 +0530

    29 mar 1

--- models/Order.js
+++ models/Order.js
@@ -46,5 +46,4 @@ const orderSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// ✅ FIXED EXPORT (VERY IMPORTANT)
-module.exports = mongoose.models.Order || mongoose.model('Order', orderSchema);
+module.exports = mongoose.model('Order', orderSchema);
